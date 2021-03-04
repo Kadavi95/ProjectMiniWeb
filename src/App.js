e040b6e51958a3734c9ec34e7c469dda8650290b
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2021-03-04 19:10:43 +0100

    added node sass, reset scss

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
+import React, {Component} from 'react'
 
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {  }
+  render() { 
+    return ( <> 
+    <p></p></> );
+  }
 }
-
+ 
 export default App;
