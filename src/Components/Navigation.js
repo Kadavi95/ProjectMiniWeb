37dfb28af699acc209cbdb9907adb5c343e760f9
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2021-03-08 16:29:24 +0100

    structure in scss file

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Navlink, Link } from "react-router-dom";
-import DarkMode from './DarkMode';
-
+import DarkMode from "./DarkMode";
+import "../sass/Navigation.scss";
 class Navigation extends Component {
   state = {
     mobile: false,
@@ -32,37 +32,83 @@ class Navigation extends Component {
   changeNightMode = () => {
     const nightMode = this.state.nightMode;
     this.setState({
-      nightMode: !nightMode 
-    })
+      nightMode: !nightMode,
+    });
   };
   render() {
     const links = this.state.links.map((item) => (
-      <li key={item.id} className={this.state.nightMode === false ? item.firstClassName : item.secondClassName}>
-        <Link to={item.link} onClick={this.closeMobileMenu} className={item.itemClassName}>
+      <li
+        key={item.id}
+        className={
+          this.state.nightMode === false
+            ? item.firstClassName
+            : item.secondClassName
+        }
+      >
+        <Link
+          to={item.link}
+          onClick={this.closeMobileMenu}
+          className={item.itemClassName}
+        >
           {item.title}
         </Link>
       </li>
     ));
-      
+
     console.log(this.state.nightMode);
     return (
-   
       <>
-        <nav className={this.state.nightMode ? "navigationContainerDay" : "navigationContainerNight"}>
-          <div onClick={this.changeNightMode} className="darkModeContainer">
-            <DarkMode  className={this.state.nightMode ? "darkModeOn" : 'darkModeOff' }/>
-          </div>
-          <div className={this.state.mobile ? 'logeContainerDay' : 'logoContainerNight'}>
-            <Link to="/" exact='true' onClick={this.closeMobileMenu}>Przychodnia Vet
-            </Link>
-          </div>
-          <div className="hamburgerMenu" onClick={this.closeMobileMenu}>
-            <i
-              className={this.state.mobile ? "fas fa-times" : "fas fa-bars"}
-            ></i>
-          </div>
-          <ul className={this.state.mobile ? 'menuActive' : 'menuUnactive'} >{links}</ul>
-        </nav>
+        <div
+          className={
+            this.state.nightMode === false
+              ? "navigationContainerDay"
+              : "navigationContainerNight"
+          }
+        >
+          <nav className='navigationWidthContainer'>
+            <div onClick={this.changeNightMode} className="darkModeContainer">
+              <DarkMode
+                className={this.state.nightMode ? "darkModeOn" : "darkModeOff"}
+              />
+            </div>
+            <div
+              className={
+                this.state.nightMode === false
+                  ? "logeContainerDay"
+                  : "logoContainerNight"
+              }
+            >
+              <Link to="/" exact="true" onClick={this.closeMobileMenu}  className={
+                this.state.nightMode === false
+                  ? "logoLinkDay"
+                  : "LogoLinkNight"
+              }>
+                Przychodnia Vet
+              </Link>
+            </div>
+            <div
+              className={
+                this.state.nightMode === false
+                  ? "hamburgerMenuDay"
+                  : "hamburgerMenuNight"
+              }
+              onClick={this.closeMobileMenu}
+            >
+              <i
+                className={this.state.mobile ? "fas fa-times" : "fas fa-bars"}
+              ></i>
+            </div>
+            <ul
+              className={`${
+                this.state.mobile ? "menuActive" : "menuUnactive"
+              } ${
+                this.state.nightMode === false ? "menuDayMode" : "menuNightMode"
+              }`}
+            >
+              {links}
+            </ul>
+          </nav>
+        </div>
       </>
     );
   }
@@ -70,15 +116,11 @@ class Navigation extends Component {
 
 export default Navigation;
 
-
-
- 
-
-{/* <i class="fas fa-bars"></i>
+{
+  /* <i class="fas fa-bars"></i>
 <i class="fas fa-times"></i>
 <i class="fab fa-facebook-f"></i>
 <i class="fab fa-instagram"></i>
 <i class="fab fa-twitter"></i>
-<i class="fas fa-paw"></i> */}
-
-
+<i class="fas fa-paw"></i> */
+}
