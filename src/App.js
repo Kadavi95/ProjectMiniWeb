e8ab375e5c49d4db1e553c02f603e2236d1512ef
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2021-03-10 00:31:22 +0100

    new Route in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Link, Navlink} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Link, Navlink, Route} from 'react-router-dom';
 
 import Navigation from './Components/Navigation';
+import homePage from './Components/pages/homePage';
 
 const App = () => {
   return ( <> 
   <Router>
     <Navigation/>
     <Switch>
-
+      <Route path='/' exact = {true} component={homePage} ></Route>
     </Switch>
   </Router>
   </> );
---
+++ src/Components/pages/homePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class homePage extends Component {
+    state = {  }
+    render() { 
+        return ( <> 
+        <p>resposywne</p>
+        </>  );
+    }
+}
+ 
+export default homePage;
