b03f2d788e9f3ff5c1ae19b6b1e5f205e7a63120
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2021-03-10 19:17:13 +0100

    logic done, adding SCSS file for photos

--- src/Components/pages/homePage.js
+++ src/Components/pages/homePage.js
@@ -2,61 +2,46 @@ import React, { Component } from "react";
 import firstPhoto from "../images/146056944_138097601473150_5983105150059591154_n (1).jpg";
 import secondPhoto from "../images/159324348_122258026456601_3731502239306874094_n.jpg";
 import thirdPhoto from "../images/20210207_150936.jpg";
+import '../../sass/homePage.scss';
 
 
-// const imagesTable = [firstPhoto, secondPhoto, thirdPhoto ];
-// console.log(imagesTable);
-// const indexMaxValue = imagesTable.length - 1;
-// const index = Math.floor(Math.random() * (indexMaxValue - 0 + 1 ) + 0);
-// console.log(index);
-// const singleImage = imagesTable[index]
+const imagesTable = [firstPhoto, secondPhoto, thirdPhoto ];
+console.log(imagesTable);
+const indexMaxValue = imagesTable.length - 1;
+const index = Math.floor(Math.random() * (indexMaxValue - 0 + 1 ) + 0);
+console.log(index);
+const singleImage = imagesTable[index]
+console.log(singleImage);
 
-// let singleImage
-// setInterval( changeIndex, 5000)
-// function changeIndex() {
-//   const imagesTable = [firstPhoto, secondPhoto, thirdPhoto];
-//   const indexMaxValue = imagesTable.length - 1;
-//   const index = Math.floor(Math.random() * (indexMaxValue - 0 + 1) + 0);
-//   singleImage = imagesTable[index];
-
-// }
 
 class homePage extends Component {
   state = {
-    image: '',
+    image: singleImage,
   };
 
   componentDidMount() {
-    setInterval(this.changePhoto, 10000);
+    setInterval(this.changePhoto, 1000);
    
   }
 
   changePhoto = () => {
-    function changeIndex() {
       const imagesTable = [firstPhoto, secondPhoto, thirdPhoto];
       const indexMaxValue = imagesTable.length - 1;
       const index = Math.floor(Math.random() * (indexMaxValue - 0 + 1) + 0);
       const singleImage = imagesTable[index];
-      const image = {
-        value: singleImage
-      }
-      return (
-        image
-      )
-
-    }
-    changeIndex()
-      console.log(image);
-    this.setState({
-       
-    })
+      this.setState({
+        image: singleImage
+      })
   };
 
   render() {
     console.log(this.state.image);
     return (
       <>
-        <img src={this.state.image} alt="obraz" />
+      <div className='homePageContainer'>
+        <img id='photo' className='singlePhoto' src={this.state.image} alt="obraz" />
+        <button className='buttonLink'></button>
+        </div>
       </>
     );
   }
