29c0c0dc1c1eb1310e9cd9d154163e0787aa8165
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2021-03-16 23:02:32 +0100

    new component with reeactHooks

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navigation from './Components/Navigation';
 import homePage from './Components/pages/homePage';
 import contactus from './Components/pages/contactUs';
 import Appointment from './Components/pages/appointment';
+import AboutUs from './Components/pages/aboutUs'
 
 const App = () => {
   return ( <> 
@@ -13,6 +14,7 @@ const App = () => {
     <Navigation/>
     <Switch>
       <Route path='/' exact = {true} component={homePage} ></Route>
+      <Route path='aboutUs' exact = {true} component = {AboutUs} />
       <Route path='/contactUs' exact = {true} component={contactus} ></Route>
       <Route path='/appointment' exact = {true} component={Appointment} />
     </Switch>
---
+++ src/Components/pages/aboutUs.js
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from 'react';
+
+
+const AboutUs = () => {
+    return ( <> 
+    </> );
+}
+ 
+export default AboutUs ;
