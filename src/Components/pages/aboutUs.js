44dc20d4e69e12aa5b003dedb41dd491f64697e8
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2021-03-17 00:43:12 +0100

    added two setStates

--- src/Components/pages/aboutUs.js
+++ src/Components/pages/aboutUs.js
@@ -7,15 +7,20 @@ const row = 3;
 const organizationsNames = fakeData.map(({ type }) => type);
 
 const AboutUs = () => {
+// indexOrganization-do przerzucania między zawartością main buttonów, domyślnie od 0(lleft)//
+//set Data -przechowuje jeden z 3 obieków, domyślnie pierwszy, meetoda setDate to zmiany//
+
   const [indexOrganization, setIndexOrganization] = useState(0);
+  const [data, setData] = useState(fakeData[0])
   console.log(indexOrganization);
+  console.log(data);
   return (
     <>
       <main className="aboutUsContainer">
-        {/* Tworzę  div'a a w nim mapuję po tablicy organizationNames. Każdy button dostaje name jako zawartość oraz onClick który wywoła setIndex po kliknięciu */}
+        {/* Tworzę  div'a a w nim mapuję po tablicy organizationNames. Każdy button dostaje name jako zawartość oraz onClick który wywoła setIndexOrganization(z indexem) po kliknięciu. Dla wyjęcia błędów, każdemu buttonowi daję ID równemu indeksowi */}
         <div className="organisationsContainer">
           {organizationsNames.map((name, i) => (
-            <button onClick={() => setIndexOrganization(i)}>{name}</button>
+            <button id={i}onClick={() => setIndexOrganization(i)}>{name}</button>
           ))}
         </div>
       </main>
