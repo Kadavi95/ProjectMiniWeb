b71156b9bc03ded13589f798421e9eb8bf3e6edf
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2021-03-17 00:07:08 +0100

    created mainButtons

--- src/Components/pages/aboutUs.js
+++ src/Components/pages/aboutUs.js
@@ -1,11 +1,26 @@
-import React, {useEffect, useState} from 'react';
-import fakeData from '../../Elements/fakeData';
+import React, { useEffect, useState } from "react";
+import fakeData from "../../Elements/fakeData";
+
+//stała równa 3//
+const row = 3;
+//Tworze tablicę zawierającą 3 stringi-każdego użyję do oddzielnego mainButtonu, kaźdy string ma swój własny index-obvious//
+const organizationsNames = fakeData.map(({ type }) => type);
 
 const AboutUs = () => {
+  const [indexOrganization, setIndexOrganization] = useState(0);
+  console.log(indexOrganization);
+  return (
+    <>
+      <main className="aboutUsContainer">
+        {/* Tworzę  div'a a w nim mapuję po tablicy organizationNames. Każdy button dostaje name jako zawartość oraz onClick który wywoła setIndex po kliknięciu */}
+        <div className="organisationsContainer">
+          {organizationsNames.map((name, i) => (
+            <button onClick={() => setIndexOrganization(i)}>{name}</button>
+          ))}
+        </div>
+      </main>
+    </>
+  );
+};
 
-return ( <> 
-    
-    </> );
-}
- 
-export default AboutUs ;
+export default AboutUs;
