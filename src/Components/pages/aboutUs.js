3a410ac9c44ece0f97799935d67122e43de63af5
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2021-03-18 01:06:21 +0100

    description in progress

--- src/Components/pages/aboutUs.js
+++ src/Components/pages/aboutUs.js
@@ -5,36 +5,36 @@ const rowNumber = 3;
 const organizationsName = fakeData.map(({ type }) => type);
 console.log(organizationsName);
 
+//komponent funkcyjny-nazwa, opis, i jego dzieci dla któ®ych iteruje po każdym twojrza dla niego diva z kluczem//
 const ListRow = ({ name, description, items }) => (
-  <div >
-    <div >
+  <div>
+    <div>
       <p>{name}</p>
       <p>{description}</p>
     </div>
-    <div >
+    <div>
       {items.map((item) => (
-        <div  style={{ marginLeft: ".3rem" }}>
-          {item}
-        </div>
+        <div key={item} >{item}</div>
       ))}
     </div>
   </div>
 );
-console.log(ListRow);
 
+//funkcja paginate-  w tym wypadku,pod parametr array pobierany drugi klucz każdego obiektu który ma w sobie nablicę obiektów, oraz pageNumber-tutaj podstawia się currentPage. Dla talicy wykonuje metodę tablicową slice- od do. Gdzie od jest równe page number - 1 * liczba rzędów, a do currentNumber * row (3). Domyślnie od 0 do 3, potem od 3 do 6, i tak dalej...//
 const paginate = (array, pageNumber) => {
 
-  console.log(array.slice((pageNumber - 1) * rowNumber));
   return array.slice((pageNumber - 1) * rowNumber, pageNumber * rowNumber);
 };
+//Komponent klasowy ze spredowaniem zamiast props, ma własną tablicę. Oblicza ilość przycisków na powstawie długość tablicy data. orgqnisations.  Następnie fukcja for do wartośći maks numberofbuttons, wpycha type numeber których użyję później do literowania. Następnie z tablicy arr, literuję mapą, tworząc dla każdego buttona, z metodą do zmiany currentPage  i + 1 dla każdego. 
 const Buttons = ({ totalItems, setCurrentPage }) => {
   const arr = [];
   const numberOfButtons = totalItems / rowNumber;
   for (let i = 0; i < numberOfButtons; i++) {
     arr.push(i);
+    console.log(i);
   }
   return arr.map((i) => (
-    <button className="switch_button" onClick={() => setCurrentPage(i + 1)}>
+    <button key={i} className="switch_button" onClick={() => setCurrentPage(i + 1)}>
       {i + 1}
     </button>
   ));
@@ -44,13 +44,13 @@ const AboutUs = () => {
   // indexOrganization-do przerzucania między zawartością main buttonów, domyślnie od 0(lleft)//
   //set Data -przechowuje jeden z 3 obieków, domyślnie pierwszy, meetoda setDate to zmiany//
   //Pieerwszy useEffeect- po pierwsze domyślnie po każdej zmiannie indexOrganization zmiani wartość currentPage na 1 (tak by przeskakiwaniu między strronami, gdy jedna ma większą zawartość od drugie nie doszło do błędów.) Po drugie ustawi obiekt wykorzystuje metodę setDate, tak by wskazywało na dany obiekt  po indexie)//
-    //Drugi useEffect ustawi odpowiednie dane, wyskozystując fukcjn paginate która przyjmuje dwa parametry- array i page number. Pod parametr 
+  //Drugi useEffect ustawi odpowiednie dane, wykorzystując funkcję paginate która przyjmuje dwa parametry- array i page number. Pod parametr pierwszy podstawiane będzie drugi klucz obiektu (zawierający tablicę obiektów), a pod drugi parametr current page//
 
   const [indexOrganization, setIndexOrganization] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
   const [data, setData] = useState(fakeData[0]);
   const [currentPageData, setCurrentPageData] = useState(null);
-  console.log(data);
+
   useEffect(() => {
     setCurrentPage(1);
     setData(fakeData[indexOrganization]);
@@ -60,14 +60,15 @@ const AboutUs = () => {
     setCurrentPageData(paginate(data.organizations, currentPage));
   }, [data, currentPage]);
 
-  console.log(indexOrganization);
+
   return (
     <>
-      <main >
-        <div >
+      <main>
+          {/* mapuję pod wcześniej utworzonej tablicy (w tym wypadku 3 obekt.) dla każdego tworzę btn  który ma key z indexu. Każdy btn ma swój classNam i metodę onCLick która ustawi indexOrganization wedle wartości indexu. To z kolei inicjować pierwszą metodę useEffect  */}
+        <div>
           {organizationsName.map((name, i) => (
             <button
-              key = {i}
+              key={i}
               className="fundations_main-button"
               onClick={() => setIndexOrganization(i)}
             >
@@ -75,14 +76,12 @@ const AboutUs = () => {
             </button>
           ))}
         </div>
-        <div >
+        <div>
           {currentPageData?.map(({ name, description, items }) => (
             <ListRow name={name} description={description} items={items} />
           ))}
         </div>
-        <div
-          style={{ display: "flex", alignItems: "center" }}
-        >
+        <div>
           <Buttons
             totalItems={data.organizations.length}
             setCurrentPage={setCurrentPage}
