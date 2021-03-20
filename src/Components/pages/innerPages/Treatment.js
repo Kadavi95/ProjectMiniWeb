6c1e143ed49bd56f2f8529c30baea320454e146e
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2021-03-20 17:35:34 +0100

    created new base

--- src/Components/pages/innerPages/Treatment.js
+++ src/Components/pages/innerPages/Treatment.js
@@ -1,7 +1,48 @@
 import React from 'react'; 
 
+const procedures = [
+    {
+        name: 'lorem',
+        price: '37',
+    },
+    {
+        name: 'lorem2',
+        price: '51',
+    },
+    {
+        name: 'lore3',
+        price: '44',
+    },
+    {
+        name: 'lorem4',
+        price: '117',
+    },
+    {
+        name: 'lorem5',
+        price: '380',
+    },
+    {
+        name: 'lorem6',
+        price: '71',
+    },
+    {
+        name: 'lorem7',
+        price: '65',
+    },
+    {
+        name: 'lorem8',
+        price: '105',
+    }
+]
+
+
+
+
+
+
+
 const Treatment = () => {
-    return ( <><p>dsa</p> </> );
+    return ( <> </> );
 }
  
 export default Treatment;
