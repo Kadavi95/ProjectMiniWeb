9f1c28f3172df033e5235694a4d33e8a5b828ff0
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2021-03-25 19:49:03 +0100

    problem solved

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,20 +2,38 @@ import React from 'react';
 const google = 'http://www.google.pl';
 const rpPl = 'http://www.rp.pl';
 const weszloCom = 'http://www.weszlo.com';
-const linksToPages = [];
+const linksToPages = ['http://www.google.pl', 'http://www.rp.pl', 'http://www.weszlo.com'];
+const linksAndIcons = [
+    {
+        links: 'http://www.google.pl',
+        icons: 'fab fa-facebook'
+    },
+    {
+        links: 'http://www.rp.pl',
+        icons: 'fab fa-facebook'
+    },
+    {
+        links: 'http://www.weszlo.com',
+        icons: 'fab fa-facebook'
+    },
+]
 
 
 
 
 const Footer = () => {
-
+    const singlePage = linksAndIcons.map(({links, icons}) => (
+        <a href={links} className={icons} target='_blank'></a>
+    ))
     return ( <>
     <div className="footer_container">
         <div className="vet_conteiner">
             <h3>Vet</h3>
         </div>
-        <div className="links_container">
+        <div className="icons_container">
+            {singlePage}
         </div>
+      
     </div>
      </>);
 }
