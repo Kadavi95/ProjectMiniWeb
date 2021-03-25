5cc7c307e3cb47f8062571899c9ba15aa92e2148
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2021-03-25 19:28:42 +0100

    ready for the last element

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,21 @@
 import React from 'react';
+const google = 'http://www.google.pl';
+const rpPl = 'http://www.rp.pl';
+const weszloCom = 'http://www.weszlo.com';
+const linksToPages = [];
+
+
 
 
 const Footer = () => {
+
     return ( <>
     <div className="footer_container">
-        
+        <div className="vet_conteiner">
+            <h3>Vet</h3>
+        </div>
+        <div className="links_container">
+        </div>
     </div>
      </>);
 }
