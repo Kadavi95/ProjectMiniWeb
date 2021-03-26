78f51fda9ac21f89208b146d877bcda5bae16c0b
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2021-03-26 16:28:22 +0100

    hover only for desktops

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,20 +1,20 @@
 import React from 'react';
-const google = 'http://www.google.pl';
-const rpPl = 'http://www.rp.pl';
-const weszloCom = 'http://www.weszlo.com';
-const linksToPages = ['http://www.facebook.pl', 'http://www.rp.pl', 'http://www.weszlo.com'];
+import '../sass/footer.scss';
 const linksAndIcons = [
     {
         links: 'http://www.facebook.pl',
-        icons: 'fab fa-facebook'
+        icons: 'fab fa-facebook',
+        artificialID: 'abc'
     },
     {
         links: 'http://www.instagram.com',
-        icons: 'fab fa-instagram'
+        icons: 'fab fa-instagram',
+        artificialID: 'abc'
     },
     {
         links: 'http://www.twitter.com',
-        icons: 'fab fa-twitter'
+        icons: 'fab fa-twitter',
+        artificialID: 'abc'
     },
 ]
 
@@ -22,8 +22,8 @@ const linksAndIcons = [
 
 
 const Footer = () => {
-    const singlePage = linksAndIcons.map(({links, icons}) => (
-        <a href={links} className={icons} target='_blank'></a>
+    const singlePage = linksAndIcons.map(({links, icons, artificialID}) => (
+        <a href={links} className={icons} id={artificialID} target='_blank'></a>
     ))
     return ( <>
     <div className="footer_container">
