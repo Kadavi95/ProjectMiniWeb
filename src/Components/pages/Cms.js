a0cf14e62bc4068d3c06a9c95baa7458948658bf
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2021-03-29 22:07:45 +0200

    table done

--- src/Components/pages/Cms.js
+++ src/Components/pages/Cms.js
@@ -26,18 +26,41 @@ const CMS = () => {
   console.log(appointments);
 
   const singleMessage = appointments.map((item, i) => (
-    <li key={i}>
-      <div>
-        <span>{item.id}</span>
-
-      </div>
-      <div>{item.messageValue}</div>
-    </li>
+    <>
+    <tr key={i} className = 'cms_tbody_firstRow'>
+        <td className='firstRow_date'>{i}</td>
+        <td className='firstRow_date'>{item.name}</td>
+        <td className='firstRow_date'>{item.surName}</td>
+        <td className='firstRow_date'>{item.day}</td>
+        <td className='firstRow_date'>{item.hour}</td>
+        <td className='firstRow_date'>{item.telephoneNumber}</td>
+    </tr>
+    <tr className="cms_tbody-secondRow">
+        <td className='secondRow_message'>Wiadomość</td>
+        <td className='secondRow_message-content'>{item.messageValue !== '' ? item.messageValue : <p>Brak uwag</p>}</td>
+    </tr>
+    </>
   ));
   return (
     <>
       <div className="cms_container">
-      
+        <div className="cms_table_container">
+            <table className='cms_table'>
+                <thead className='cms_table-thead'>
+                    <tr className='cms_table-firstrow'>
+                        <th className='cms_table-th'>ID</th>
+                        <th className='cms_table-th'>Imię</th>
+                        <th className='cms_table-th'>Nazwisko</th>
+                        <th className='cms_table-th'>Dzień</th>
+                        <th className='cms_table-th'>Godzina</th>
+                        <th className='cms_table-th'>Number tel</th>
+                    </tr>
+                </thead>
+                <tbody className='cms_table-tbody'>
+                    {singleMessage}
+                </tbody>
+            </table>
+        </div>
       </div>
     </>
   );
