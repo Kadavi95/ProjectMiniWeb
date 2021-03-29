3c5a0c46a8e88fe680e73fcb4e05848293db8c86
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2021-03-30 00:11:44 +0200

    added scroll for CMS table

--- src/Components/pages/Cms.js
+++ src/Components/pages/Cms.js
@@ -14,11 +14,9 @@ const CMS = () => {
           const obj = {
             id: doc.id,
             ...doc.data(),
-            
           };
           arr.push(obj);
           console.log(obj);
-
         });
         setAppointments(arr);
       });
@@ -27,39 +25,39 @@ const CMS = () => {
 
   const singleMessage = appointments.map((item, i) => (
     <>
-    <tr key={i} className = 'cms_tbody_firstRow'>
-        <td className='firstRow_date'>{i}</td>
-        <td className='firstRow_date'>{item.name}</td>
-        <td className='firstRow_date'>{item.surName}</td>
-        <td className='firstRow_date'>{item.day}</td>
-        <td className='firstRow_date'>{item.hour}</td>
-        <td className='firstRow_date'>{item.telephoneNumber}</td>
-    </tr>
-    <tr className="cms_tbody-secondRow">
-        <td className='secondRow_message'>Wiadomość</td>
-        <td className='secondRow_message-content'>{item.messageValue !== '' ? item.messageValue : <p>Brak uwag</p>}</td>
-    </tr>
+      <tr key={i} className="cms_tbody_firstRow">
+        <td className="firstRow_date">{i}</td>
+        <td className="firstRow_date">{item.name}</td>
+        <td className="firstRow_date">{item.surName}</td>
+        <td className="firstRow_date">{item.day}</td>
+        <td className="firstRow_date">{item.hour}</td>
+        <td className="firstRow_date">{item.telephoneNumber}</td>
+      </tr>
+      <tr className="cms_tbody-secondRow">
+        <td className="secondRow_message">Wiadomość</td>
+        <td className="secondRow_message-content">
+          {item.messageValue !== "" ? item.messageValue : <p>Brak uwag</p>}
+        </td>
+      </tr>
     </>
   ));
   return (
     <>
       <div className="cms_container">
         <div className="cms_table_container">
-            <table className='cms_table'>
-                <thead className='cms_table-thead'>
-                    <tr className='cms_table-firstrow'>
-                        <th className='cms_table-th'>ID</th>
-                        <th className='cms_table-th'>Imię</th>
-                        <th className='cms_table-th'>Nazwisko</th>
-                        <th className='cms_table-th'>Dzień</th>
-                        <th className='cms_table-th'>Godzina</th>
-                        <th className='cms_table-th'>Telefon</th>
-                    </tr>
-                </thead>
-                <tbody className='cms_table-tbody'>
-                    {singleMessage}
-                </tbody>
-            </table>
+          <table className="cms_table">
+            <thead className="cms_table-thead">
+              <tr className="cms_table-firstrow">
+                <th className="cms_table-th">ID</th>
+                <th className="cms_table-th">Imię</th>
+                <th className="cms_table-th">Nazwisko</th>
+                <th className="cms_table-th">Dzień</th>
+                <th className="cms_table-th">Godzina</th>
+                <th className="cms_table-th">Telefon</th>
+              </tr>
+            </thead>
+            <tbody className="cms_table-tbody">{singleMessage}</tbody>
+          </table>
         </div>
       </div>
     </>
