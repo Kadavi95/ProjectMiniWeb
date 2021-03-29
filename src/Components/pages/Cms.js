d1c984ddbeaace005762cf9ab8c2a8f33039cf35
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2021-03-29 17:22:03 +0200

    Table to be done

--- src/Components/pages/Cms.js
+++ src/Components/pages/Cms.js
@@ -1,16 +1,43 @@
-import React, {useState, useEffect} from "react";
-import { firestore } from '../firebase';
-import '../../sass/cms.scss';
+import React, { useState, useEffect } from "react";
+import { firestore } from "../firebase";
+import "../../sass/cms.scss";
 
 const CMS = () => {
-const [appointments, setAppointments] = useState([]);
-useEffect(() => {
-    firestore.collection("appoint")
-})
+  const [appointments, setAppointments] = useState([]);
+  useEffect(() => {
+    firestore
+      .collection("appointments")
+      .get()
+      .then((querySnapshot) => {
+        const arr = [];
+        querySnapshot.forEach((doc) => {
+          const obj = {
+            id: doc.id,
+            ...doc.data(),
+            
+          };
+          arr.push(obj);
+          console.log(obj);
+
+        });
+        setAppointments(arr);
+      });
+  }, []);
+  console.log(appointments);
+
+  const singleMessage = appointments.map((item, i) => (
+    <li key={i}>
+      <div>
+        <span>{item.id}</span>
+
+      </div>
+      <div>{item.messageValue}</div>
+    </li>
+  ));
   return (
     <>
       <div className="cms_container">
-
+      
       </div>
     </>
   );
