0dacf13bef7f691f64f7a4b054a841616e0e7054
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2021-03-30 14:32:04 +0200

    looking for some solutions

--- src/Components/pages/homePage.js
+++ src/Components/pages/homePage.js
@@ -21,7 +21,23 @@ class homePage extends Component {
   changePhoto = () => {
     const imagesTable = [firstPhoto, secondPhoto, thirdPhoto];
     const indexMaxValue = imagesTable.length - 1;
-    const index = Math.floor(Math.random() * (indexMaxValue - 0 + 1) + 0);
+ 
+    // let index = Math.floor(Math.random() * (indexMaxValue - 0 + 1) + 0)
+    let number = 0;
+    if (number <= 3){
+      setInterval(function(){
+        let falseFlag = true
+        if (number < 3){
+          number++
+        }
+          if (number === 3 && number > -1) {
+            number--
+          }
+        console.log(number);
+      },1000)
+    }
+    
+
     const singleImage = imagesTable[index];
     this.setState({
       image: singleImage,
@@ -54,3 +70,12 @@ class homePage extends Component {
 }
 
 export default homePage;
+
+
+
+// if (index < indexMaxValue){
+//   setTimeout(function(){
+//     console.log('Hello');
+//     index++
+//   },1000)
+// }
